06863f127a3cf04ba622c53819bee6682bb7a698
tlhathollo17-del <tlhathollo17@gmail.com>
2025-12-18 15:28:04 +0200

    Style.css

---
+++ style.css
@@ -0,0 +1,21 @@
+function generateTicket() {
+    document.getElementById("tEvent").innerText =
+        document.getElementById("eventName").value;
+
+    document.getElementById("tDate").innerText =
+        document.getElementById("date").value;
+
+    document.getElementById("tTime").innerText =
+        document.getElementById("time").value;
+
+    document.getElementById("tVenue").innerText =
+        document.getElementById("venue").value;
+
+    document.getElementById("tHost").innerText =
+        document.getElementById("host").value;
+}
+
+function downloadPDF() {
+    const ticket = document.getElementById("ticket");
+    html2pdf().from(ticket).save("Invitation_Ticket.pdf");
+}
